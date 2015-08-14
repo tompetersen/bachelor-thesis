0b375d71a9c3fc8157c7f52f7cbcc56ec7ce79bc
Tom Petersen <toomy1@gmx.de>
2015-08-14 02:11:03 +0200

    Refactored enums again

--- Source/jProtocol/src/jProtocol/tls12/model/values/TlsContentType.java
+++ Source/jProtocol/src/jProtocol/tls12/model/values/TlsContentType.java
@@ -1,35 +1,27 @@
 package jProtocol.tls12.model.values;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum TlsContentType {
-		ChangeCipherSpec,
-		Alert,
-		Handshake,
-		ApplicationData;
-	
-	private static Map<TlsContentType, Byte> _types;
-	
-	static {
-		_types = new HashMap<TlsContentType, Byte>();
-		
-		_types.put(TlsContentType.ChangeCipherSpec, 	(byte)20);
-		_types.put(TlsContentType.Alert, 				(byte)21);
-		_types.put(TlsContentType.Handshake, 			(byte)22);
-		_types.put(TlsContentType.ApplicationData, 	(byte)23);
-	}
+		ChangeCipherSpec	((byte)20),
+		Alert				((byte)21),
+		Handshake			((byte)22),
+		ApplicationData		((byte)23);
 	
 	public static TlsContentType contentTypeFromValue(byte b) {
 		for (TlsContentType t : TlsContentType.values()) {
-			if (b == _types.get(t)) {
+			if (b == t.getValue()) {
 				return t;
 			}
 		}
 		throw new IllegalArgumentException("No ContentType for value " + b + "!");
 	}
 	
+	private byte _value;
+	
+	private TlsContentType(byte value) {
+		_value = value;
+	}
+	
 	public byte getValue() {
-		return _types.get(this);
+		return _value;
 	}
 }
--- Source/jProtocol/src/jProtocol/tls12/model/values/TlsHandshakeType.java
+++ Source/jProtocol/src/jProtocol/tls12/model/values/TlsHandshakeType.java
@@ -1,47 +1,33 @@
 package jProtocol.tls12.model.values;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum TlsHandshakeType {
-		hello_request,
-		client_hello,
-		server_hello,
-		certificate,
-		server_key_exchange,
-		certificate_request,
-		server_hello_done,
-		certificate_verify,
-		client_key_exchange,
-		finished;
-	
-	private static Map<TlsHandshakeType, Byte> _types;
-	
-	static {
-		_types = new HashMap<TlsHandshakeType, Byte>();
-		
-		_types.put(TlsHandshakeType.hello_request,			(byte)0);
-		_types.put(TlsHandshakeType.client_hello,			(byte)1);
-		_types.put(TlsHandshakeType.server_hello,			(byte)2);
-		_types.put(TlsHandshakeType.certificate,			(byte)11);
-		_types.put(TlsHandshakeType.server_key_exchange,	(byte)12);
-		_types.put(TlsHandshakeType.certificate_request,	(byte)13);
-		_types.put(TlsHandshakeType.server_hello_done,		(byte)14);
-		_types.put(TlsHandshakeType.certificate_verify,	(byte)15);
-		_types.put(TlsHandshakeType.client_key_exchange,	(byte)16);
-		_types.put(TlsHandshakeType.finished,				(byte)20);
-	}
+	hello_request 			((byte)0),
+	client_hello 			((byte)1),
+	server_hello 			((byte)2),
+	certificate 			((byte)11),
+	server_key_exchange		((byte)12),
+	certificate_request 	((byte)13),
+	server_hello_done 		((byte)14),
+	certificate_verify 		((byte)15),
+	client_key_exchange 	((byte)16),
+	finished 				((byte)20);
 	
 	public static TlsHandshakeType handshakeTypeFromValue(byte b) {
 		for (TlsHandshakeType t : TlsHandshakeType.values()) {
-			if (b == _types.get(t)) {
+			if (b == t.getValue()) {
 				return t;
 			}
 		}
 		throw new IllegalArgumentException("No HandshakeType for value " + b + "!");
 	}
 	
+	private byte _value;
+	
+	private TlsHandshakeType(byte value) {
+		_value = value;
+	}
+	
 	public byte getValue() {
-		return _types.get(this);
+		return _value;
 	}
 }
